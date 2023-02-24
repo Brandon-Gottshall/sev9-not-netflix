034ba141e1ef70537b406fc08334805f451fd1fd
Sarah Johnson <kitteyandkat@aim.com>
2023-02-23 19:35:38 -0500

    modal updates

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,4 +1,25 @@
-import React, {useState, useEffect} from "react"
-//import modal from modal
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
 
-//onclick
+const SummaryModal = () => {
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
+  return (
+    <div>
+      <button className ='infomation-tag' onClick={onOpenModal}>Information</button>
+      <Modal open={open} onClose={onCloseModal} center>
+        <h2>Film Title</h2>
+        <p>This is where the video goes</p>
+        <p>Film Synopsis</p>
+        <p>Run Time</p>
+      </Modal>
+    </div>
+  );
+};
+
+export default SummaryModal
