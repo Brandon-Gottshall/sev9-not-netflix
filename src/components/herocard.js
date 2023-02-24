909bf5d1aa54d907aeae66ffa69f7f17e19d2303
Hennc94 <h3nn3ssyc@yahoo.com>
2023-02-24 16:37:44 -0500

    modal imported

--- src/components/herocard.js
+++ src/components/herocard.js
@@ -1,38 +1,38 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import HoverVideoPlayer from 'react-hover-video-player';
 import ReactPlayer from 'react-player'
+import SummaryModal from './Modal.js';
 
 const HeroImage = () => {
-    const [id, getID] = useState();
+  const [id, getID] = useState();
 
-        useEffect(() => {
-        fetch('https://api.themoviedb.org/3/trending/all/day?api_key=62be9389e81a8c75366a852f32ce210a')
-            .then(res => res.json())
-            .then(res => {
-            getID(res.results[0])
-            })
-        }, [])
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/trending/all/day?api_key=62be9389e81a8c75366a852f32ce210a')
+      .then(res => res.json())
+      .then(res => {
+        getID(res.results[0])
+      })
+  }, [])
 
-    const [key, getKey] = useState();
-        useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/${id?.id}/videos?api_key=62be9389e81a8c75366a852f32ce210a&language=en-US`)
-            .then(res => res.json())
-            .then(res => {
-            getKey(res.results[2])
-            })
-        }, [])
-    let videoURL = `https://www.youtube.com/watch?v${key?.key}`
-return(
+  const [key, getKey] = useState();
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id?.id}/videos?api_key=62be9389e81a8c75366a852f32ce210a&language=en-US`)
+      .then(res => res.json())
+      .then(res => {
+        getKey(res.results[0])
+      })
+  }, [])
+  let videoURL = `https://www.youtube.com/watch?v=${key?.key}`
+
+  return (
     <div>
-   <h1>${key?.key}</h1>
+      <ReactPlayer url={videoURL} />
+      <SummaryModal className='hero-modal' id={id?.id} />
     </div>
-)
+  )
 }
 
 export default HeroImage
-
-
-
 
 
 
