45086a2819ff8fc7e9b82f5f78c22bc73e5517db
Sarah Johnson <kitteyandkat@aim.com>
2023-02-23 21:11:51 -0500

    modal update

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,22 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
 import infobutton from './info-button.png'
 
-const SummaryModal = () => {
-  const [open, setOpen] = useState(false);
 
+const SummaryModal = ({id}) => {
+  const [open, setOpen] = useState(false);
+ 
   const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
+  const [info, setInfo] = useState();
 
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/${id}/movie/13?api_key=62be9389e81a8c75366a852f32ce210a&language=en-US`)
+    .then(res => res.json())
+    .then(res =>{
+    setInfo(res)
+  })
+},[])
   return (
     <div>
       <button className ='info-tag' onClick={onOpenModal}><img src={infobutton} height='15px' width='15px'></img></button>
       <Modal open={open} onClose={onCloseModal} center>
-        <h2>Film Title</h2>
+        <h2>Film Title: {info?.original_title}</h2>
         <p>This is where the video goes</p>
-        <p>Film Synopsis</p>
-        <p>Run Time</p>
+        <p>{info?.overview}</p>
+        <p>Run Time: {info?.runtime} minutes</p>
+ 
       </Modal>
     </div>
   );
