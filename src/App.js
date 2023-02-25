0d5864b8c147735c76ab605e7753bf1b9a90d58b
nebulamarie <mepps1115@gmail.com>
2023-02-25 14:03:01 -0500

    sev9-not-netflix-project-update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import './App.css';
-import HeroImage from './Components/hero';
-import MovieSection from './components/MovieSection';
-import React, { useState, useEffect } from 'react';
-
-
-
+import "./App.css";
+import HeroImage from "./Components/hero";
+import MovieSection from "./components/MovieSection";
+import React, { useState, useEffect } from "react";
+import fetchCalls from "./Helpers/SectionList";
 
 function App() {
-  const [movies, setMovies] = useState();
+  const [actionMovies, setActionMovies] = useState();
+  const [comedyMovies, setComedyMovies] = useState();
+  const [crimeMovies, setCrimeMovies] = useState();
+  const [horrorMovies, setHorrorMovies] = useState();
   useEffect(() => {
-      fetch(`https://api.themoviedb.org/3/movie/popular?api_key=114d88d72a1471cf241d7a43bb60a917&language=en-US&page=1`)
-      .then(res => res.json())
-      .then (res => {
-        setMovies(res.results)
-      })
-  },[])
+    setActionMovies(fetchCalls.fetchActionMovies());
+    setComedyMovies(fetchCalls.fetchComedyMovies());
+    setCrimeMovies(fetchCalls.fetchCrimeMovies());
+    setHorrorMovies(fetchCalls.fetchHorrorMovies());
+  }, []);
   return (
     <div className="main-container">
-      <div className='hero-image'>
-        <HeroImage/>
-        <MovieSection movies={movies}  />
+      <div className="hero-image">
+        <HeroImage />
+        <div className="movie-sections">
+          <h1>Horror</h1>
+          <MovieSection movies={horrorMovies} genre={"Horror"} />
+          <h1>Action</h1>
+          <MovieSection movies={actionMovies} genre={"Action"} />
+          <h1>Crime</h1>
+          <MovieSection movies={crimeMovies} genre={"Crime"} />
+          <h1>Comedy</h1>
+          <MovieSection movies={comedyMovies} genre={"Comedy"} />
+        </div>
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/MovieSection.js
+++ src/components/MovieSection.js
@@ -2,15 +2,14 @@ import React from 'react'
 
 export default function MovieSection(props) {
 console.log(props.movies)
-    return (
-        <div>
-            <h1>Movie Title</h1>
-            {props.movies.map(movie =>{
+    return(
+        <div className="movie-section">
+
+            {props.movies?props.movies.map(movie =>{
                 return (
-                    <div>{movie.original_title}</div>
+                    <div className="single-movie">{movie.original_title}</div>
                 )
-            })}
+            }): ''}
         </div>
     )
 }
-
