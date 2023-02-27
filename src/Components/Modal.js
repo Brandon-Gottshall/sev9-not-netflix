0b0079449765b8a7cf43635a25b09c38f2e23a9a
Sarah Johnson <kitteyandkat@aim.com>
2023-02-27 17:50:29 -0500

    optimization

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -5,31 +5,37 @@ import infobutton from './info-button.png'
 import ReactPlayer from 'react-player'
 
 
-export default function SummaryModal  ({ id = 13, className }) {
-  const [open, setOpen] = useState(false);
+export default function SummaryModal  ({ id = 13, className, setCardModal, cardData }) {
+  // const [open, setOpen] = useState(false);
   const [info, setInfo] = useState();
-  const [key, getKey] = useState();
+  const [key, setKey] = useState();
   let videoURL = `https://www.youtube.com/watch?v=${key?.key}`
 
-  const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setOpen(false);
+  // const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setCardModal(false);
 
   useEffect(() => {
+    // if a movie is passed in, don't look up
+    console.log(cardData, id);
+    if(cardData) {
+      setInfo(cardData);
+      id = cardData.id;
+    } else {
     fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=62be9389e81a8c75366a852f32ce210a&language=en-US`)
       .then(res => res.json())
       .then(res => {
         setInfo(res)
       })
+    }
   fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=62be9389e81a8c75366a852f32ce210a&language=en-US`)
       .then(res => res.json())
       .then(res => {
-      getKey(res.results[0])
+      setKey(res.results[0])
       })
   }, [])
   return (
     <div className={className}>
-      <button className='info-tag' onClick={onOpenModal}><img src={infobutton}></img></button>
-      <Modal open={open} onClose={onCloseModal} center>
+      <Modal open={true} onClose={onCloseModal} center>
         <h2>Film Title: {info?.original_title}</h2>
         <ReactPlayer url={videoURL}/>
         <p>{info?.overview}</p>
