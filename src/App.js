9e46e419e44d683f69a8b0dc8efbb7953eebc4d1
Sarah Johnson <kitteyandkat@aim.com>
2023-02-27 17:55:46 -0500

    app.js updtae

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import SummaryModal from './Components/Modal';
 import HeroImage from './Components/hero';
+import { useState, useEffect } from 'react';
 
 
 function App() {
+
+  //when this is active the modal will be shown 
+  const [cardModal, setCardModal] = useState();
+  
   return (
     <div className="main-container">
+      {cardModal && <SummaryModal cardData={cardModal} setCardModal={setCardModal}/>}
       <div className='hero-image'>
-        <HeroImage/>
+        <HeroImage setCardModal={setCardModal}/>
       </div>
     </div>
   );
