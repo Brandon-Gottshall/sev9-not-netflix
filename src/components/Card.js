a143e7bd93b08bb1af9063d07051606967ea31e5
nebulamarie <mepps1115@gmail.com>
2023-03-02 20:00:42 -0500

    sev9-not-netflix project update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,13 @@
 import React from "react";
 
- export default function Card(props) {
-    const {title, posterPath} = props
-    
+export default function Card(props) {
+    const { posterPath } = props
+
     return (
-        <div className ='Overlay'>
-        <div className="Card">
-        {/* <h1>{title}</h1> */}
-        <img src={'https://image.tmdb.org/t/p/w500' + posterPath} alt="movieCard" />
-        
-      </div>
-      </div>
-
-
+        <div className='Overlay'>
+            <div className="Card">
+                <img src={'https://image.tmdb.org/t/p/w500' + posterPath} alt="movieCard" />
+            </div>
+        </div>
     );
-  }
-  
+}
