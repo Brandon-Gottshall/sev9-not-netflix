f25de789134ba5fda98d522ecd554c9d95fae341
Brandon🤖 <brandon@effortlessdevelopment.com>
2023-03-04 06:54:45 -0500

    Rebase and merge section component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import "./App.css";
 import HeroImage from "./Components/hero";
-import MovieSection from "./components/MovieSection";
+import MovieSection from "./Components/MovieSection";
 import React, { useState, useEffect } from "react";
 import fetchCalls from "./Helpers/SectionList";
 
 function App() {
-  const [actionMovies, setActionMovies] = useState();
-  const [comedyMovies, setComedyMovies] = useState();
-  const [crimeMovies, setCrimeMovies] = useState();
-  const [horrorMovies, setHorrorMovies] = useState();
-  
-  
+  const [actionMovies, setActionMovies] = useState([]);
+  const [comedyMovies, setComedyMovies] = useState([]);
+  const [crimeMovies, setCrimeMovies] = useState([]);
+  const [horrorMovies, setHorrorMovies] = useState([]);
   useEffect(() => {
-    setActionMovies(fetchCalls.fetchActionMovies());
-    setComedyMovies(fetchCalls.fetchComedyMovies());
-    setCrimeMovies(fetchCalls.fetchCrimeMovies());
-    setHorrorMovies(fetchCalls.fetchHorrorMovies());
+    fetchCalls.fetchActionMovies().then((data) => setActionMovies(data));
+    fetchCalls.fetchComedyMovies().then((data) => setComedyMovies(data));
+    fetchCalls.fetchCrimeMovies().then((data) => setCrimeMovies(data));
+    fetchCalls.fetchHorrorMovies().then((data) => setHorrorMovies(data));
   }, []);
   return (
     <div className="main-container">
       <div className="hero-image">
         <HeroImage />
-        <div className="movie-sections">
-          <h1>Horror</h1>
-          <MovieSection movies={horrorMovies} genre={"Horror"} />
-          <h1>Action</h1>
-          <MovieSection movies={actionMovies} genre={"Action"} />
-          <h1>Crime</h1>
-          <MovieSection movies={crimeMovies} genre={"Crime"} />
-          <h1>Comedy</h1>
-          <MovieSection movies={comedyMovies} genre={"Comedy"} />
-        </div>
+      </div>
+
+      <div className="movie-sections">
+        <h1>Horror</h1>
+        <MovieSection movies={horrorMovies} genre={"Horror"} />
+        <h1>Action</h1>
+        <MovieSection movies={actionMovies} genre={"Action"} />
+        <h1>Comedy</h1>
+        <MovieSection movies={comedyMovies} genre={"Comedy"} />
+        <h1>Crime</h1>
+        <MovieSection movies={crimeMovies} genre={"Crime"} />
       </div>
     </div>
   );
--- src/Components/hero.js
+++ src/Components/hero.js
@@ -1,47 +1,75 @@
-import React, {useState, useEffect} from "react"
-import ReactPlayer from 'react-player'
-import SummaryModal from './Modal.js';
-
-
+import React, { useState, useEffect } from "react";
+import ReactPlayer from "react-player";
+import SummaryModal from "./Modal.js";
+import infobutton from './info-button.png'
 
 const HeroImage = () => {
-    const [id, getID] = useState();
-    const [key, getKey] = useState();
-    const [playing, setPlaying] =useState(false);
-    const [mute, unMute] = useState(false);
+  const [id, setID] = useState();
+  const [key, setKey] = useState();
+  const [videoURL, setVideoURL] = useState();
+  const [playing, setPlaying] = useState(false);
+  const [mute, unMute] = useState(false);
+  const [showModel, setShowModel] = useState(false);
 
-function startPlay(){
-    setPlaying(true)
-}
-function stopPlay(){
-    setPlaying(false)
-}
+  function startPlay() {
+    setPlaying(true);
+  }
+  function stopPlay() {
+    setPlaying(false);
+  }
 
-    useEffect( () => {
-         fetch('https://api.themoviedb.org/3/trending/all/day?api_key=62be9389e81a8c75366a852f32ce210a')
-            .then(res => res.json())
-            .then(res => {
-                getID(res.results[0]);
-                return res.results[0];
-            })
-            .then(trendingId =>
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/trending/all/day?api_key=62be9389e81a8c75366a852f32ce210a"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setID(res.results[0].id);
+        console.log(res.results);
+        let url = `https://api.themoviedb.org/3/${res.results[0].media_type}/${res.results[0].id}/videos?api_key=62be9389e81a8c75366a852f32ce210a&language=en-US`;
+        console.log(url);
+        fetch(url)
+          .then((res) => res.json())
+          .then((res) => {
+            setKey(res.results[0].key);
+          });
+      });
+  }, []);
 
-        fetch(`https://api.themoviedb.org/3/movie/${trendingId.id}/videos?api_key=62be9389e81a8c75366a852f32ce210a&language=en-US`) 
-        .then(res => res.json())
-            .then(res => {
-            getKey(res.results[0])
-            }))
-        }, [])
+  useEffect(() => {
+    console.log('key', key)
+    setVideoURL(
+      key
+        ? `https://www.youtube.com/watch?v=${key}/modestbranding=1&showinfo=0&fs=0&rel=0`
+        : ""
+    );
+    console.log(videoURL);
+  }, [key]);
 
-    let videoURL = `https://www.youtube.com/watch?v=${key?.key}?modestbranding=1&showinfo=0&fs=0&rel=0`
+  const hideModal = () => { setShowModel(false) }
+  const showModal = () => { setShowModel(true) }
 
-return(
-    <div className="hero-row row">
-        {/* <iframe width="560" height="315" src= {`https://www.youtube.com/embed/${key?.key}?modestbranding=1&showinfo=0`}frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
-   <ReactPlayer muted = {true} controls = {true} playing= {playing} onMouseOver= { startPlay } onMouseLeave= {stopPlay} url= {videoURL} loop = {true} height = '100%' width = '100%' />
-   {id && <SummaryModal className='hero-modal' id={id?.id}/>}
-    </div>
-)
-}
+  return (
+    <>
+      <div className="hero-row row">
+        {videoURL && (
+          <ReactPlayer
+            muted={true}
+            controls={true}
+            playing={playing}
+            onMouseOver={startPlay}
+            onMouseLeave={stopPlay}
+            url={videoURL}
+            loop={true}
+            height="100%"
+            width="100vw"
+          />
+        )}
+      </div>
+      {showModel && <SummaryModal className="hero-modal" id={id} hideModal={hideModal}/>}
+      <button className='info-tag' onClick={showModal}><img src={infobutton}></img></button>
+    </>
+  );
+};
 
-export default HeroImage
+export default HeroImage;
