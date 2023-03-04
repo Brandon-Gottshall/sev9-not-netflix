02c62295110880a99b19b86c483bb1df58942860
Brandon🤖 <brandon@effortlessdevelopment.com>
2023-03-04 08:32:34 -0500

    updates

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,53 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import 'react-responsive-modal/styles.css';
-import { Modal } from 'react-responsive-modal';
-// import infobutton from './info-button.png'
-import ReactPlayer from 'react-player'
+import React, { useState, useEffect } from "react";
+import ReactPlayer from "react-player";
+import SummaryModal from "./Modal.js";
+import infobutton from "./info-button.png";
 
-
-export default function SummaryModal({ id = 13, className, setCardModalData, cardModalData }) {
-  // const [open, setOpen] = useState(false);
-  const [info, setInfo] = useState();
+const Hero = ({setCardModalData}) => {
+  const [id, setID] = useState();
+  const [mediaType, setMediaType] = useState();
   const [key, setKey] = useState();
-  let videoURL = `https://www.youtube.com/watch?v=${key?.key}`
+  const [videoURL, setVideoURL] = useState();
+  const [playing, setPlaying] = useState(false);
+  const [mute, unMute] = useState(false);
 
-  // const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setCardModalData(false);
+  function startPlay() {
+    setPlaying(true);
+  }
+  function stopPlay() {
+    setPlaying(false);
+  }
 
   useEffect(() => {
-    // if a movie is passed in, don't look up
-    let mediaType
-    console.log(cardModalData, id);
-    console.log(Object.keys(cardModalData).length);
-    if (Object.keys(cardModalData).length > 2) {
-      setInfo(cardModalData);
-      id = cardModalData.id;
-    } else {
-      if (cardModalData.id) { 
-        id = cardModalData.id
-        mediaType = cardModalData.mediaType
-      }
-      fetch(`https://api.themoviedb.org/3/${mediaType}/${id}?api_key=62be9389e81a8c75366a852f32ce210a&language=en-US`)
-        .then(res => res.json())
-        .then(res => {
-          setInfo(res)
-        })
-    }
-    fetch(`https://api.themoviedb.org/3/${mediaType}/${id}/videos?api_key=62be9389e81a8c75366a852f32ce210a&language=en-US`)
-      .then(res => res.json())
-      .then(res => {
-        setKey(res.results[0])
-      })
-  }, [])
-  return (
-    <div className={className}>
-      <Modal open={true} onClose={onCloseModal} center>
-        <h2>Film Title: {info?.original_title}</h2>
-        <ReactPlayer url={videoURL} />
-        <p>{info?.overview}</p>
-        <p>Run Time: {info?.runtime} minutes</p>
+    fetch(
+      "https://api.themoviedb.org/3/trending/all/day?api_key=62be9389e81a8c75366a852f32ce210a"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setID(res.results[0].id);
+        setMediaType(res.results[0].media_type);
+        console.log(res.results);
+        let url = `https://api.themoviedb.org/3/${res.results[0].media_type}/${res.results[0].id}/videos?api_key=62be9389e81a8c75366a852f32ce210a&language=en-US`;
+        console.log(url);
+        fetch(url)
+          .then((res) => res.json())
+          .then((res) => {
+            setKey(res.results[0].key);
+          });
+      });
+  }, []);
 
-      </Modal>
+  useEffect(() => {
+    console.log("key", key);
+    setVideoURL(
+      key
+        ? `https://www.youtube.com/watch?v=${key}/modestbranding=1&showinfo=0&fs=0&rel=0`
+        : ""
+    );
+    console.log(videoURL);
+  }, [key]);
+
+  return (
+    <div className="hero-row row">
+      <ReactPlayer
+        muted={true}
+        controls={true}
+        playing={playing}
+        onMouseOver={startPlay}
+        onMouseLeave={stopPlay}
+        url={videoURL}
+        loop={true}
+        height="100%"
+        width="100%"
+      />
+      <button className="info-tag" onClick={() => {
+        console.log("id", id)
+        setCardModalData({id: id, mediaType: mediaType})
+    }}>
+        <img alt='info button' src={infobutton}></img>
+      </button>
     </div>
   );
 };
+
+export default Hero;
---
+++ src/Components/hero.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import ReactPlayer from "react-player";
+import SummaryModal from "./Modal.js";
+import infobutton from "./info-button.png";
+
+const Hero = ({setCardModalData}) => {
+  const [id, setID] = useState();
+  const [mediaType, setMediaType] = useState();
+  const [key, setKey] = useState();
+  const [videoURL, setVideoURL] = useState();
+  const [playing, setPlaying] = useState(false);
+  const [mute, unMute] = useState(false);
+
+  function startPlay() {
+    setPlaying(true);
+  }
+  function stopPlay() {
+    setPlaying(false);
+  }
+
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/trending/all/day?api_key=62be9389e81a8c75366a852f32ce210a"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setID(res.results[0].id);
+        setMediaType(res.results[0].media_type);
+        console.log(res.results);
+        let url = `https://api.themoviedb.org/3/${res.results[0].media_type}/${res.results[0].id}/videos?api_key=62be9389e81a8c75366a852f32ce210a&language=en-US`;
+        console.log(url);
+        fetch(url)
+          .then((res) => res.json())
+          .then((res) => {
+            setKey(res.results[0].key);
+          });
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log("key", key);
+    setVideoURL(
+      key
+        ? `https://www.youtube.com/watch?v=${key}/modestbranding=1&showinfo=0&fs=0&rel=0`
+        : ""
+    );
+    console.log(videoURL);
+  }, [key]);
+
+  return (
+    <div className="hero-row row">
+      <ReactPlayer
+        muted={true}
+        controls={true}
+        playing={playing}
+        onMouseOver={startPlay}
+        onMouseLeave={stopPlay}
+        url={videoURL}
+        loop={true}
+        height="100%"
+        width="100%"
+      />
+      <button className="info-tag" onClick={() => {
+        console.log("id", id)
+        setCardModalData({id: id, mediaType: mediaType})
+    }}>
+        <img alt='info button' src={infobutton}></img>
+      </button>
+    </div>
+  );
+};
+
+export default Hero;
