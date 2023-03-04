19c75126e6033819d0c028645d1ad7d4d2ba24b2
shawn bailey <shwnmbailey@gmail.com>
2023-03-04 14:34:54 -0500

    Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,11 +2,16 @@ import React from 'react';
 import '../Header.css';
 
 function Header() {
-  return (
-    <div className="header">
-      <h1>Not Netflix</h1>
-    </div>
-  );
-}
-
-export default Header;
+    return (
+      <div className="header">
+        <h1>Not Netflix</h1>
+        <div className="header__switch">
+          <button className="header__switch-button">Movies</button>
+          <button className="header__switch-button">TV</button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Header;
+  
