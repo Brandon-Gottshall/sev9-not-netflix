91c7ac6243a275e76107ce972e7ad0775fc2e8ce
Sarah Johnson <kitteyandkat@aim.com>
2023-03-08 17:22:06 -0500

    fuzzy searchbar debouncing

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,43 +1,61 @@
 import React, {useState, useEffect} from "react"
 import MovieSection from "./MovieSection";
 
+let searchDebouncer;
 const SearchBar = () => {
-const [query, setQuery] = useState("");
-const [searchResults, setSearchResults] = useState([]);
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
 const handleSearchChange = (e) => {
+  console.log(e.target.value);
   setQuery(e.target.value);
+  searchQuery(e);
 }
 
 const resetInputField = () => {
   setQuery("")
 }
 
-const SearchQuery = (e) => {
+const debounce = (fn) =>{
+  // if a search debouncer exists, cancel that request
+  if(searchDebouncer){
+    clearTimeout(searchDebouncer);
+    console.log(searchDebouncer, 'cancelled');
+  }
+  //call the function provided in 175 milliseconds
+  searchDebouncer = setTimeout(fn , 175);
+  console.log(searchDebouncer);
+}
+const searchQuery = (e) => {
   e.preventDefault();
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=62be9389e81a8c75366a852f32ce210a&language=en-US&query=${query}&page=1&include_adult=false`)
-       .then(res => res.json())
-       .then(res => {
-           setSearchResults(res.results);
-          console.log(res.results)
-           return res.results;
-          })
-  resetInputField()
+  //debounce if request is already happening, cancel and then after delay, fire off the request
+  debounce( () => {
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=62be9389e81a8c75366a852f32ce210a&language=en-US&query=${query}&page=1&include_adult=false`)
+        .then(res => res.json())
+        .then(res => {
+            setSearchResults(res.results);
+          // after request is finished, searchDebouncer should not exist
+           searchDebouncer = null
+            return res.results;
+            })
+        })
+  // resetInputField()
 }
 
   
 
   return(
-    <div>
+    <div className="search-bar-container">
       <form className="search-bar">
-        <input
+        <input className="search-field"
           value={query}
           onChange={handleSearchChange}
           type="text"
-        />
-        <input onClick={SearchQuery} type="submit" value="SEARCH" />
-        <MovieSection movies={searchResults} genre={query}/>
+          placeholder="Search for a Movie, TV Show, Person..." required
+        /> 
+        <button className ="search-bar-button" onClick={searchQuery} type="submit">Search</button>
       </form>
+        <MovieSection className ="search-movie-section" movies={searchResults} genre={query}/>
       
     </div>
   )
